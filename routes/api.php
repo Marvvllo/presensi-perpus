8a77418460e09f892b450c83ecd53325093b7ba4
Muhammad Sahal <msahalmahfuuzh@gmail.com>
2023-09-25 15:06:28 +0800

    menambahkan middleware

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::controller(AuthController::class)->middleware('auth:sanctum')->group(['prefix' => 'auth'], function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+      Route::get('logout', 'logout');
+      Route::get('user', 'user');
+
 });
